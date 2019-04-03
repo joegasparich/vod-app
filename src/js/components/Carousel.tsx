08047c261290c57220a6e69f43169faf5141884a
Joe <joe.gasparich@fusion5.co.nz>
2019-04-03 13:27:20 +1300

    Carousel is now keyboard navigable

--- src/js/components/Carousel.tsx
+++ src/js/components/Carousel.tsx
@@ -7,7 +7,9 @@ import MovieTile from "./MovieTile";
 interface IProps {
 	movies: IMovie[];
 }
-interface IState {}
+interface IState {
+	index: number;
+}
 
 const Styles = css`
 	display: flex;
@@ -16,15 +18,57 @@ const Styles = css`
 `;
 
 class Carousel extends React.Component<IProps, IState> {
+	// Initial state
+	public readonly state: IState = {
+		index: 0,
+	};
+
 	public render(): JSX.Element {
-		const movieList = this.props.movies.map((movie: IMovie) => <MovieTile movie={movie} />);
+		const movieList = this.props.movies.map((movie: IMovie, index: number) => (
+			<MovieTile
+				key={movie.id}
+				movie={movie}
+				focused={this.state.index === index}
+				handleSelect={this.handleSelect}
+			/>
+		));
 
 		return (
-			<div id="carousel" css={Styles}>
+			<div id="carousel" css={Styles} tabIndex={0} onKeyDown={this.handleKeyDown}>
 				{movieList}
 			</div>
 		);
 	}
+
+	private handleKeyDown = (event: React.KeyboardEvent) => {
+		switch (event.key) {
+			case "ArrowRight":
+				event.preventDefault();
+				if (this.state.index < this.props.movies.length - 1) {
+					this.setState({ index: this.state.index + 1 });
+				}
+				break;
+			case "ArrowLeft":
+				event.preventDefault();
+				if (this.state.index > 0) {
+					this.setState({ index: this.state.index - 1 });
+				}
+				break;
+			default:
+				break;
+		}
+	};
+
+	private handleSelect = (event: React.SyntheticEvent<HTMLDivElement, Event>) => {
+		this.props.movies.map((movie: IMovie, index: number) => {
+			if (movie.id === event.currentTarget.id) {
+				this.setState({
+					index,
+				});
+				return;
+			}
+		});
+	};
 }
 
 export default Carousel;
--- src/js/components/MovieTile.tsx
+++ src/js/components/MovieTile.tsx
@@ -5,31 +5,51 @@ import { IMovie, IImage } from "../types/MovieData";
 
 interface IProps {
 	movie: IMovie;
+	focused: boolean;
+	handleSelect: (event: React.SyntheticEvent<HTMLDivElement, Event>) => void;
 }
 interface IState {}
 
 const Styles = css`
 	margin: 1rem;
+	min-width: 214px;
+	overflow: hidden;
 
 	.title {
 		text-align: center;
+		white-space: nowrap;
+		margin: 0;
+	}
+
+	&:focus {
+		border: 2px solid #5599ff;
 	}
 `;
 
 class MovieTile extends React.Component<IProps, IState> {
+	// Refs
+	private ref: React.RefObject<HTMLDivElement> = React.createRef();
+
+	// Lifecycle Methods
+	public componentDidUpdate() {
+		if (this.props.focused && this.ref.current) {
+			this.ref.current.focus();
+		}
+	}
+
 	public render(): JSX.Element {
 		const image: IImage | undefined = this.props.movie.images.find((i: IImage) => i.type === "cover");
-		if (!image) {
-			return (
-				<div id="movie-tile" css={Styles}>
-					<p>{this.props.movie.title}</p>
-				</div>
-			);
-		}
 
 		return (
-			<div id="movie-tile" css={Styles}>
-				<img src={image.url} alt={this.props.movie.title} />
+			<div
+				id={this.props.movie.id}
+				className="movie-tile"
+				css={Styles}
+				tabIndex={-1}
+				ref={this.ref}
+				onFocus={this.props.handleSelect}
+			>
+				{image && <img src={image.url} alt={this.props.movie.title} />}
 				<p className="title">{this.props.movie.title}</p>
 			</div>
 		);
